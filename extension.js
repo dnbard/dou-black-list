955464dff8de7babf26948fe9d1a51b05138bc05
Alex Bardanov <dnbard@gmail.com>
2015-04-11 18:38:29 +0300

    black list in articles (/lenta/**/* endpoint)

--- extension.js
+++ extension.js
@@ -1,14 +1,47 @@
 (function(window){
     var comments = document.querySelectorAll('.comment'),
+        article = document.querySelector('.b-content-page') || document.querySelector('.cell.g-right-shadowed.mobtab-maincol'),
         localStorageKey = '__black_list',
         commentIsHiddenString = 'Комментарий скрыт',
+        articleIsHiddenString = 'Статья скрыта',
         commentTitleAdd = 'Добавить в черный список',
         commentTitleRemove = 'Удалить из черного списка',
         blackList = JSON.parse(localStorage.getItem(localStorageKey) || '[]'),
         blackListIndexes = createBlackListIndexes(blackList),
-        comment, authorElement, author, buttonNode, textNode;
+        comment, authorElement, author, buttonNode, textNode, articleAuthor, articleNode;
 
     console.log('Black List %o', blackList);
+
+    if (article){
+        articleAuthor = article.querySelector('.author .name a').innerText;
+
+        if (isInBlackList(articleAuthor)){
+            articleNode = article.querySelector('article.b-typo');
+            articleNode.setAttribute('data-hidden', 'true');
+            articleNode.setAttribute('data-text', articleNode.innerText);
+            articleNode.innerText = articleIsHiddenString;
+            articleNode.classList.add('comment-hidden');
+        } else {
+            buttonNode = document.createElement('BUTTON');
+            buttonNode.setAttribute('class', 'list-action');
+            buttonNode.innerText = '+';
+            buttonNode.setAttribute('title', commentTitleAdd);
+
+            article.querySelector('.author .name').appendChild(buttonNode);
+            buttonNode.onclick = function(event){
+                articleNode = article.querySelector('article.b-typo');
+                articleNode.setAttribute('data-hidden', 'true');
+                articleNode.setAttribute('data-text', articleNode.innerText);
+                articleNode.innerText = articleIsHiddenString;
+                articleNode.classList.add('comment-hidden');
+
+                addToBlackList(articleAuthor);
+                hideAllCommentsByAuthor(articleAuthor);
+
+                event.target.style.display = "none";
+            }
+        }
+    }
 
     if (comments.length > 0){
         for(var i = 0; i < comments.length; i ++){
