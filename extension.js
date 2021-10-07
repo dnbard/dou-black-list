d1254e2c2a69cb8f486b1344349cc9d2f0faa9c3
Victor Musiienko <victor.musiienko@auth0.com>
2021-10-06 23:21:05 -0400

    .gitignore: - add package-lock.json to ignore files, in order to keep minimal repo size
    extension.ts:
    tsconfig.json:
    - add typescript
    
    package.json:
    - add typescript and prettier
    
    extension.js:
    - rewrite the whole plugin with new shiny tech and nicer buttons

--- extension.js
+++ extension.js
@@ -1,204 +1,158 @@
-(function(window){
-    var comments = document.querySelectorAll('.comment'),
-        article = document.querySelector('.b-content-page') || document.querySelector('.cell.g-right-shadowed.mobtab-maincol'),
-        localStorageKey = '__black_list',
-        commentIsHiddenString = 'Комментарий скрыт',
-        articleIsHiddenString = 'Статья скрыта',
-        commentTitleAdd = 'Добавить в черный список',
-        commentTitleRemove = 'Удалить из черного списка',
-        blackList = JSON.parse(localStorage.getItem(localStorageKey) || '[]'),
-        blackListIndexes = createBlackListIndexes(blackList),
-        comment, authorElement, author, buttonNode, textNode, articleAuthor, articleNode;
+const style = `
+  ._ban_button {
+    border: none;
+    background: transparent;
+    cursor: pointer;
+    opacity: 0.2;
+  }
+  ._ban_button:active {
+    background: #f0c0c0;
+  }
+  ._ban_button:hover {
+    opacity: 1;
+  }
+  ._banned {
+    opacity: 0.5;
+  }
+`;
+function attachStyles() {
+    const styletag = document.createElement("style");
+    styletag.innerText = style;
+    document.head.appendChild(styletag);
+}
+const SELECTORS = {
+    comment: ".comment",
+    author: ".b-post-author > a",
+    text: ".comment_text",
+    expand: ".expand-thread",
+    link: ".comment-link",
+};
+const STORAGE_KEY = "__dou_black_list__";
+const HIDDEN_COMMENT = `<div class="_banned">
+  <div class="b-post-author">
+    <a class="avatar">
+      <img
+        class="g-avatar"
+        alt="avatar"
+        src="https://s.dou.ua/img/avatars/80x80_966.png"
+        width="25"
+        height="25"
+      />
+      Banned user
+    </a>
+  </div>
 
-    console.log('Black List %o', blackList);
-
-    if (article){
-        articleAuthor = article.querySelector('.author .name a').innerText;
-
-        if (isInBlackList(articleAuthor)){
-            articleNode = article.querySelector('article.b-typo');
-            articleNode.setAttribute('data-hidden', 'true');
-            articleNode.setAttribute('data-text', articleNode.innerText);
-            articleNode.innerText = articleIsHiddenString;
-            articleNode.classList.add('comment-hidden');
-        } else {
-            buttonNode = document.createElement('BUTTON');
-            buttonNode.setAttribute('class', 'list-action');
-            buttonNode.innerText = '+';
-            buttonNode.setAttribute('title', commentTitleAdd);
-
-            article.querySelector('.author .name').appendChild(buttonNode);
-            buttonNode.onclick = function(event){
-                articleNode = article.querySelector('article.b-typo');
-                articleNode.setAttribute('data-hidden', 'true');
-                articleNode.setAttribute('data-text', articleNode.innerText);
-                articleNode.innerText = articleIsHiddenString;
-                articleNode.classList.add('comment-hidden');
-
-                addToBlackList(articleAuthor);
-                hideAllCommentsByAuthor(articleAuthor);
-
-                event.target.style.display = "none";
-            }
-        }
+  <div class="comment_text b-typo">Hidden content, click to show</div>
+</div>
+`;
+function getTextElement(comment) {
+    return comment.querySelectorAll(SELECTORS.text)[0];
+}
+function getText(comment) {
+    return getTextElement(comment).innerText;
+}
+function getAuthorElement(comment) {
+    return comment.querySelectorAll(SELECTORS.author)[0];
+}
+function getAuthor(comment) {
+    return getAuthorElement(comment)?.innerText.trim();
+}
+function getLink(comment) {
+    return comment.querySelectorAll(SELECTORS.link)[0];
+}
+(function () {
+    const storage = JSON.parse(localStorage.getItem(STORAGE_KEY) || "{}");
+    const index = {};
+    function updateStorage(key, value) {
+        storage[key] = value;
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(storage));
     }
-
-    if (comments.length > 0){
-        for(var i = 0; i < comments.length; i ++){
-            comment = comments[i];
-            authorElement = comment.querySelector('.avatar');
-
-            if (authorElement){
-                author = authorElement.innerText;
-                buttonNode = document.createElement('BUTTON');
-                buttonNode.setAttribute('class', 'list-action');
-
-                if (!isInBlackList(author)){
-                    buttonNode.innerText = '+';
-                    buttonNode.setAttribute('title', commentTitleAdd);
-
-                    comment.querySelector('.b-post-author').appendChild(buttonNode);
-                    buttonNode.onclick = getAddToBlackListHandler(author);
-                } else {
-                    buttonNode.innerText = '-';
-                    buttonNode.setAttribute('title', commentTitleRemove);
-
-                    comment.querySelector('.b-post-author').appendChild(buttonNode);
-                    buttonNode.onclick = getRemoveFromBlackListHandler(author);
-
-                    hideComment(comment.querySelector('.text'));
+    function readAllComments() {
+        return [...document.querySelectorAll(SELECTORS.comment)];
+    }
+    function isCommentFromBanned(comment) {
+        return !!storage[getAuthor(comment)];
+    }
+    function addBanButton(comment) {
+        const author = getAuthorElement(comment);
+        if (!author) {
+            return;
+        }
+        const existingBanButton = author.parentElement.querySelectorAll("._ban_button")[0];
+        if (existingBanButton) {
+            author.parentElement.removeChild(existingBanButton);
+        }
+        const button = document.createElement("button");
+        button.classList.add("_ban_button");
+        button.innerText = isCommentFromBanned(comment) ? "😇" : "🤡";
+        button.title = isCommentFromBanned(comment) ? "unban" : "ban";
+        button.onclick = (e) => {
+            e.stopPropagation();
+            const authorName = getAuthor(comment);
+            const yes = confirm(`${isCommentFromBanned(comment) ? "unban" : "ban"} ${authorName}?`);
+            if (yes) {
+                updateStorage(authorName, !storage[authorName]);
+                const commentsByAuthor = index[authorName];
+                commentsByAuthor.forEach((comment) => {
+                    addBanButton(comment);
+                    hideContentIfNeeded(comment);
+                });
+                console.log(`Updated ${commentsByAuthor.length} comments`);
+            }
+        };
+        author.parentElement.appendChild(button);
+    }
+    function hideContentIfNeeded(comment) {
+        const text = getTextElement(comment);
+        if (!text) {
+            return;
+        }
+        if (isCommentFromBanned(comment)) {
+            comment.setAttribute("data-banned", comment.innerHTML);
+            comment.innerHTML = HIDDEN_COMMENT;
+            comment.onclick = (e) => {
+                e.stopPropagation();
+                const content = comment.getAttribute("data-banned");
+                if (content) {
+                    comment.removeAttribute("data-banned");
+                    comment.innerHTML = content;
+                    addBanButton(comment);
+                    comment.onclick = () => { };
                 }
-            }
-        }
-    }
-
-    function isCommentHidden(commentText){
-        return commentText.getAttribute('data-hidden') === 'true';
-    }
-
-    function hideComment(commentText){
-        if (isCommentHidden(commentText)){
-            return;
-        }
-
-        commentText.setAttribute('data-hidden', 'true');
-        commentText.setAttribute('data-text', commentText.innerText);
-        commentText.innerText = commentIsHiddenString;
-        commentText.classList.add('comment-hidden');
-    }
-
-    function showComment(commentText){
-        if (!isCommentHidden(commentText)){
-            return;
-        }
-
-        commentText.innerText = commentText.getAttribute('data-text');
-        commentText.setAttribute('data-hidden', 'false');
-        commentText.setAttribute('data-text', '');
-        commentText.classList.remove('comment-hidden');
-    }
-
-    function hideAllCommentsByAuthor(authorName){
-        if (comments.length > 0){
-            for(var i = 0; i < comments.length; i ++){
-                comment = comments[i];
-                authorElement = comment.querySelector('.avatar');
-                if (authorElement){
-                    author = authorElement.innerText;
-
-                    if (author === authorName){
-                        hideComment(comment.querySelector('.text'));
-                        buttonNode = comment.querySelector('.list-action');
-
-                        buttonNode.innerText = '-';
-                        buttonNode.setAttribute('title', commentTitleRemove);
-                        buttonNode.onclick = getRemoveFromBlackListHandler(authorName);
-                    }
+                else {
+                    comment.setAttribute("data-banned", comment.innerHTML);
+                    comment.innerHTML = HIDDEN_COMMENT;
                 }
+            };
+        }
+        else {
+            const content = comment.getAttribute("data-banned");
+            if (content) {
+                comment.removeAttribute("data-banned");
+                comment.innerHTML = content;
+                addBanButton(comment);
+                comment.onclick = () => { };
             }
         }
     }
-
-    function showAllCommentsByAuthor(authorName){
-        if (comments.length > 0){
-            for(var i = 0; i < comments.length; i ++){
-                comment = comments[i];
-                authorElement = comment.querySelector('.avatar');
-                if (authorElement){
-                    author = authorElement.innerText;
-
-                    if (author === authorName){
-                        showComment(comment.querySelector('.text'));
-                        buttonNode = comment.querySelector('.list-action');
-
-                        buttonNode.innerText = '+';
-                        buttonNode.setAttribute('title', commentTitleAdd);
-                        buttonNode.onclick = getAddToBlackListHandler(authorName);
-                    }
-                }
-            }
-        }
-    }
-
-    function getAddToBlackListHandler(author){
-        return function(event){
-            var buttonNode = event.target;
-
-            addToBlackList(author);
-            hideAllCommentsByAuthor(author);
-        }
-    }
-
-    function getRemoveFromBlackListHandler(author){
-        return function(event){
-            removeFromBlackList(author);
-            showAllCommentsByAuthor(author)
-        }
-    }
-
-    function isInBlackList(author){
-        return !!blackListIndexes[author];
-    }
-
-    function createBlackListIndexes(blackList){
-        var blackListIndexes = {};
-
-        if (typeof blackList !== 'object' || typeof blackList.length !== 'number'){
-            throw new Error('Invalid argument: blackList must be an array');
-        }
-
-        for(var i = 0; i < blackList.length; i ++){
-            blackListIndexes[blackList[i]] = true;
-        }
-
-        return blackListIndexes;
-    }
-
-    function addToBlackList(author){
-        if (isInBlackList(author)){
+    function indexOne(comment) {
+        const authorName = getAuthor(comment);
+        if (!authorName) {
             return;
         }
-
-        blackListIndexes[author] = true;
-        blackList.push(author);
-        localStorage.setItem(localStorageKey, JSON.stringify(blackList));
-
-        console.log('BLACK LIST >> %s added', author);
-    }
-
-    function removeFromBlackList(author){
-        var index;
-
-        if (!isInBlackList(author)){
+        const text = getText(comment);
+        if (!text) {
             return;
         }
-
-        blackListIndexes[author] = false;
-
-        index = blackList.indexOf(author);
-        blackList.splice(index, 1);
-        localStorage.setItem(localStorageKey, JSON.stringify(blackList));
-
-        console.log('BLACK LIST >> %s removed', author);
+        if (!index[authorName]) {
+            index[authorName] = [];
+        }
+        index[authorName].push(comment);
     }
-})(this);
+    attachStyles();
+    readAllComments().forEach((comment) => {
+        indexOne(comment);
+        addBanButton(comment);
+        hideContentIfNeeded(comment);
+    });
+})();
---
+++ extension.ts
@@ -0,0 +1,174 @@
+type DouComment = HTMLElement;
+
+// language=CSS
+const style = `
+  ._ban_button {
+    border: none;
+    background: transparent;
+    cursor: pointer;
+    opacity: 0.2;
+  }
+  ._ban_button:active {
+    background: #f0c0c0;
+  }
+  ._ban_button:hover {
+    opacity: 1;
+  }
+  ._banned {
+    opacity: 0.5;
+  }
+`;
+function attachStyles() {
+  const styletag = document.createElement("style");
+  styletag.innerText = style;
+  document.head.appendChild(styletag);
+}
+const SELECTORS = {
+  comment: ".comment",
+  author: ".b-post-author > a",
+  text: ".comment_text",
+  expand: ".expand-thread",
+  link: ".comment-link",
+};
+const STORAGE_KEY = "__dou_black_list__";
+const HIDDEN_COMMENT = `<div class="_banned">
+  <div class="b-post-author">
+    <a class="avatar">
+      <img
+        class="g-avatar"
+        alt="avatar"
+        src="https://s.dou.ua/img/avatars/80x80_966.png"
+        width="25"
+        height="25"
+      />
+      Banned user
+    </a>
+  </div>
+
+  <div class="comment_text b-typo">Hidden content, click to show</div>
+</div>
+`;
+function getTextElement(comment: DouComment) {
+  return comment.querySelectorAll(SELECTORS.text)[0] as HTMLElement;
+}
+function getText(comment: DouComment) {
+  return getTextElement(comment).innerText;
+}
+function getAuthorElement(comment: DouComment): HTMLElement {
+  return comment.querySelectorAll(SELECTORS.author)[0] as HTMLElement;
+}
+function getAuthor(comment: DouComment) {
+  return getAuthorElement(comment)?.innerText.trim();
+}
+function getLink(comment: DouComment) {
+  return comment.querySelectorAll(SELECTORS.link)[0] as HTMLElement;
+}
+(function () {
+  const storage: Record<string, boolean> = JSON.parse(
+    localStorage.getItem(STORAGE_KEY) || "{}"
+  );
+  const index: Record<string, Array<DouComment>> = {};
+
+  function updateStorage(key, value) {
+    storage[key] = value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(storage));
+  }
+
+  function readAllComments(): DouComment[] {
+    // @ts-ignore
+    return [...document.querySelectorAll(SELECTORS.comment)];
+  }
+
+  function isCommentFromBanned(comment: DouComment) {
+    return !!storage[getAuthor(comment)];
+  }
+
+  function addBanButton(comment: DouComment) {
+    const author = getAuthorElement(comment);
+    if (!author) {
+      return;
+    }
+    const existingBanButton =
+      author.parentElement.querySelectorAll("._ban_button")[0];
+    if (existingBanButton) {
+      author.parentElement.removeChild(existingBanButton);
+    }
+    const button = document.createElement("button");
+    button.classList.add("_ban_button");
+    button.innerText = isCommentFromBanned(comment) ? "😇" : "🤡";
+    button.title = isCommentFromBanned(comment) ? "unban" : "ban";
+    button.onclick = (e) => {
+      e.stopPropagation();
+      const authorName = getAuthor(comment);
+      const yes = confirm(
+        `${isCommentFromBanned(comment) ? "unban" : "ban"} ${authorName}?`
+      );
+      if (yes) {
+        updateStorage(authorName, !storage[authorName]);
+        const commentsByAuthor = index[authorName];
+        commentsByAuthor.forEach((comment) => {
+          addBanButton(comment);
+          hideContentIfNeeded(comment);
+        });
+        console.log(`Updated ${commentsByAuthor.length} comments`);
+      }
+    };
+    author.parentElement.appendChild(button);
+  }
+
+  function hideContentIfNeeded(comment: DouComment) {
+    const text = getTextElement(comment);
+    if (!text) {
+      return;
+    }
+    if (isCommentFromBanned(comment)) {
+      comment.setAttribute("data-banned", comment.innerHTML);
+      comment.innerHTML = HIDDEN_COMMENT;
+      comment.onclick = (e) => {
+        e.stopPropagation();
+        const content = comment.getAttribute("data-banned");
+        if (content) {
+          // unhide
+          comment.removeAttribute("data-banned");
+          comment.innerHTML = content;
+          addBanButton(comment);
+          comment.onclick = () => {};
+        } else {
+          // hide again
+          comment.setAttribute("data-banned", comment.innerHTML);
+          comment.innerHTML = HIDDEN_COMMENT;
+        }
+      };
+    } else {
+      // unban clicked
+      const content = comment.getAttribute("data-banned");
+      if (content) {
+        comment.removeAttribute("data-banned");
+        comment.innerHTML = content;
+        addBanButton(comment);
+        comment.onclick = () => {};
+      }
+    }
+  }
+
+  function indexOne(comment: DouComment) {
+    const authorName = getAuthor(comment);
+    if (!authorName) {
+      return;
+    }
+    const text = getText(comment);
+    if (!text) {
+      return;
+    }
+    if (!index[authorName]) {
+      index[authorName] = [];
+    }
+    index[authorName].push(comment);
+  }
+  attachStyles();
+  readAllComments().forEach((comment) => {
+    indexOne(comment);
+    addBanButton(comment);
+    hideContentIfNeeded(comment);
+  });
+})();
