57b33d0807d88ec5d89c9fae37b81cbe8204218c
sdwvit <sdwvit@inet.ua>
2021-11-20 12:11:05 -0500

    attach stats
    bug:
    server throws 502 sometimes due to rate limits

--- extension.js
+++ extension.js
@@ -28,6 +28,7 @@ const getAuthor = (comment) => {
     return getAuthorElement(comment)?.href?.match(/users\/(.+)\//)?.[1];
 };
 const getStorage = () => JSON.parse(localStorage.getItem(STORAGE_KEY) || "{}");
+const getUrl = (username) => `https://dou.ua/users/${username}/`;
 (() => {
     const storage = getStorage();
     const index = {};
@@ -36,7 +37,7 @@ const getStorage = () => JSON.parse(localStorage.getItem(STORAGE_KEY) || "{}");
         storage[key] = value;
         localStorage.setItem(STORAGE_KEY, JSON.stringify(storage));
     }
-    function addBanButton(comment) {
+    function addBanButtonAndInfo(comment) {
         const author = getAuthorElement(comment);
         if (!getAuthor(comment)) {
             return;
@@ -57,13 +58,25 @@ const getStorage = () => JSON.parse(localStorage.getItem(STORAGE_KEY) || "{}");
                 updateStorage(authorName, !storage[authorName]);
                 const commentsByAuthor = index[authorName];
                 commentsByAuthor.forEach((comment) => {
-                    addBanButton(comment);
+                    addBanButtonAndInfo(comment);
                     hideContentIfNeeded(comment);
                 });
                 console.log(`Updated ${commentsByAuthor.length} comments`);
             }
         };
         author.parentElement.appendChild(button);
+        const infoBlock = document.createElement("span");
+        const registration = document.createElement("span");
+        const stats = index[getAuthor(comment)].stats;
+        if (!stats) {
+            return;
+        }
+        registration.innerHTML = stats.registration;
+        infoBlock.appendChild(stats.activities[0]);
+        infoBlock.appendChild(stats.activities[1]);
+        infoBlock.appendChild(registration);
+        infoBlock.className = "_ban_infoblock";
+        author.parentElement.appendChild(infoBlock);
     }
     function hideContentIfNeeded(comment) {
         const text = getTextElement(comment);
@@ -79,7 +92,7 @@ const getStorage = () => JSON.parse(localStorage.getItem(STORAGE_KEY) || "{}");
                 if (content) {
                     comment.removeAttribute("data-banned");
                     comment.innerHTML = content;
-                    addBanButton(comment);
+                    addBanButtonAndInfo(comment);
                     comment.onclick = () => { };
                 }
                 else {
@@ -93,12 +106,12 @@ const getStorage = () => JSON.parse(localStorage.getItem(STORAGE_KEY) || "{}");
             if (content) {
                 comment.removeAttribute("data-banned");
                 comment.innerHTML = content;
-                addBanButton(comment);
+                addBanButtonAndInfo(comment);
                 comment.onclick = () => { };
             }
         }
     }
-    function indexOne(comment) {
+    async function indexOne(comment) {
         const authorName = getAuthor(comment);
         if (!authorName) {
             return;
@@ -111,11 +124,37 @@ const getStorage = () => JSON.parse(localStorage.getItem(STORAGE_KEY) || "{}");
             index[authorName] = [];
         }
         index[authorName].push(comment);
+        if (!index[authorName].stats) {
+            index[authorName].stats = await fetchStats(authorName);
+        }
+    }
+    async function fetchStats(username) {
+        const response = await fetch(getUrl(username));
+        if (!response.ok) {
+            return;
+        }
+        const text = await response.text();
+        const html = document.createElement("html");
+        html.innerHTML = text;
+        return {
+            registration: html
+                .querySelectorAll(".status-info")[0]
+                .innerText.replace(/\t/g, "")
+                .trim()
+                .split("\n")
+                .slice(-1)[0],
+            activities: [
+                ...html.querySelectorAll(".b-content-menu")[0].childNodes,
+            ]
+                .filter((e) => e.innerText)
+                .slice(1, 3)
+                .map((e) => e.children[0]),
+        };
     }
     console.time(STORAGE_KEY);
-    [...document.querySelectorAll(SELECTORS.comment)].forEach((comment) => {
-        indexOne(comment);
-        addBanButton(comment);
+    [...document.querySelectorAll(SELECTORS.comment)].forEach(async (comment) => {
+        await indexOne(comment);
+        addBanButtonAndInfo(comment);
         hideContentIfNeeded(comment);
     });
     console.timeEnd(STORAGE_KEY);
--- extension.ts
+++ extension.ts
@@ -1,3 +1,9 @@
+type Stats = {
+  stats: {
+    registration: string;
+    activities: Element[];
+  };
+};
 type DouComment = HTMLElement;
 
 const SELECTORS = {
@@ -34,10 +40,11 @@ const getAuthor = (comment: DouComment) => {
 
 const getStorage = (): Record<string, boolean> =>
   JSON.parse(localStorage.getItem(STORAGE_KEY) || "{}");
+const getUrl = (username) => `https://dou.ua/users/${username}/`;
 
 (() => {
   const storage = getStorage();
-  const index: Record<string, Array<DouComment>> = {};
+  const index: Record<string, Array<DouComment> & Stats> = {};
   const isCommentFromBanned = (comment: DouComment) =>
     !!storage[getAuthor(comment)];
 
@@ -46,7 +53,7 @@ const getStorage = (): Record<string, boolean> =>
     localStorage.setItem(STORAGE_KEY, JSON.stringify(storage));
   }
 
-  function addBanButton(comment: DouComment) {
+  function addBanButtonAndInfo(comment: DouComment) {
     const author = getAuthorElement(comment);
     if (!getAuthor(comment)) {
       return;
@@ -70,13 +77,25 @@ const getStorage = (): Record<string, boolean> =>
         updateStorage(authorName, !storage[authorName]);
         const commentsByAuthor = index[authorName];
         commentsByAuthor.forEach((comment) => {
-          addBanButton(comment);
+          addBanButtonAndInfo(comment);
           hideContentIfNeeded(comment);
         });
         console.log(`Updated ${commentsByAuthor.length} comments`);
       }
     };
     author.parentElement.appendChild(button);
+    const infoBlock = document.createElement("span");
+    const registration = document.createElement("span");
+    const stats = index[getAuthor(comment)].stats;
+    if (!stats) {
+      return;
+    }
+    registration.innerHTML = stats.registration;
+    infoBlock.appendChild(stats.activities[0]);
+    infoBlock.appendChild(stats.activities[1]);
+    infoBlock.appendChild(registration);
+    infoBlock.className = "_ban_infoblock";
+    author.parentElement.appendChild(infoBlock);
   }
 
   function hideContentIfNeeded(comment: DouComment) {
@@ -94,7 +113,7 @@ const getStorage = (): Record<string, boolean> =>
           // unhide
           comment.removeAttribute("data-banned");
           comment.innerHTML = content;
-          addBanButton(comment);
+          addBanButtonAndInfo(comment);
           comment.onclick = () => {};
         } else {
           // hide again
@@ -108,13 +127,13 @@ const getStorage = (): Record<string, boolean> =>
       if (content) {
         comment.removeAttribute("data-banned");
         comment.innerHTML = content;
-        addBanButton(comment);
+        addBanButtonAndInfo(comment);
         comment.onclick = () => {};
       }
     }
   }
 
-  function indexOne(comment: DouComment) {
+  async function indexOne(comment: DouComment) {
     const authorName = getAuthor(comment);
     if (!authorName) {
       return;
@@ -124,18 +143,47 @@ const getStorage = (): Record<string, boolean> =>
       return;
     }
     if (!index[authorName]) {
-      index[authorName] = [];
+      index[authorName] = [] as typeof index[string];
     }
     index[authorName].push(comment);
+    if (!index[authorName].stats) {
+      index[authorName].stats = await fetchStats(authorName);
+    }
+  }
+
+  async function fetchStats(username): Promise<Stats["stats"]> {
+    const response = await fetch(getUrl(username));
+    if (!response.ok) {
+      return;
+    }
+    const text = await response.text();
+    const html = document.createElement("html");
+    html.innerHTML = text;
+    return {
+      registration: html
+        .querySelectorAll<HTMLDivElement>(".status-info")[0]
+        .innerText.replace(/\t/g, "")
+        .trim()
+        .split("\n")
+        .slice(-1)[0],
+      activities: (
+        [
+          ...html.querySelectorAll(".b-content-menu")[0].childNodes,
+        ] as HTMLLIElement[]
+      )
+        .filter((e) => e.innerText)
+        .slice(1, 3)
+        .map((e) => e.children[0]),
+    };
   }
 
   console.time(STORAGE_KEY);
   [...document.querySelectorAll(SELECTORS.comment)].forEach(
-      (comment: DouComment) => {
-        indexOne(comment);
-        addBanButton(comment);
-        hideContentIfNeeded(comment);
-      }
+    async (comment: DouComment) => {
+      await indexOne(comment);
+      addBanButtonAndInfo(comment);
+      hideContentIfNeeded(comment);
+    }
   );
   console.timeEnd(STORAGE_KEY);
 })();
